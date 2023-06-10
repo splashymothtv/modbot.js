769c7054c3f437e829ad2a775825420b183fba1b
splashymothtv <75794879+splashymothtv@users.noreply.github.com>
2023-06-10 21:49:41 +0200

    Update bot.js
    Added a Welcome Message Embed

--- bot.js
+++ bot.js
@@ -1,57 +1,71 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, MessageManager, PermissionsBitField, Permissions, Embed, EmbedBuilder, Events, Collection } = require("discord.js");
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const welcome = require("./welcome.js");
+const client = new Client({ intents: 32767 });
+
+
+const keepAlive = require("./server.js");
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(__dirname, "commands");
-	const files = fs.readdirSync(path.resolve(__dirname, 'commands'));
-	const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+  const commandsPath = path.join(__dirname, "commands");
+  const files = fs.readdirSync(path.resolve(__dirname, 'commands'));
+  const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
 }
 
 client.once(Events.ClientReady, () => {
-	console.log('Ready!');
+  console.log('Ready!');
 
-	welcome(client)
 });
+
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+  const command = client.commands.get(interaction.commandName);
 
-	console.log(interaction);
+  console.log(interaction);
 
-	if (interaction.commandName === 'alive') {
-		return interaction.reply(`I am alive!`);
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+  try {
+    await command.execute(interaction, client);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  }
 });
 
+client.on(Events.GuildMemberAdd, async (member) => {
+  const { user, guild } = member;
+  const welcomeChannel = member.guild.channels.cache.get('your-welcomeChannelId');
+  const verificationChannel = member.guild.channels.cache.get('your-verificationChannelId');
+  const rulesChannel = member.guild.channels.cache.get('your-rulesChannelId');
+  const welcomeEmbed = new EmbedBuilder()
+    .setColor('Purple')
+    .setTitle(`**Welcome to ${guild.name}**, ${member.user.tag}!`)
+    .setDescription(`Please **verify yourself** in <#verificationChannelId> , and read the **<#rulesChannelId>**`)
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+    .setTimestamp(Date.now())
+
+  welcomeChannel.send({ embeds: [welcomeEmbed]  });
+})
+
 client.login(token);
+keepAlive();
