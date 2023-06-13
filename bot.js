91ec8b9fd1a42db9c5421058e34836d08a998763
splashymothtv <75794879+splashymothtv@users.noreply.github.com>
2023-06-13 12:39:06 +0200

    Update bot.js
    Added a status array

--- bot.js
+++ bot.js
@@ -28,8 +28,41 @@ for (const folder of commandFolders) {
 }
 
 client.once(Events.ClientReady, () => {
-  console.log('Ready!');
+      console.log(`${client.user.tag} is alive!`);
+        client.pickPresence = async () => {
+        const statusArray = [
+            {
+                type: ActivityType.Listening,
+                content: '/commands',
+                status: 'online'
+            },
+            {
+                type: ActivityType.Watching,
+                content: `over ${client.guilds.cache.size} servers!`,
+                status: 'online'
+            },
+            {
+                type: ActivityType.Streaming,
+                content: `https://twitch.tv/splashymothtv`,
+                status: 'online'
+            }
 
+        ];
+
+        const option = Math.floor(Math.random() * statusArray.length);
+
+        client.user.setPresence({
+            activities: [
+                {
+                    name: statusArray[option].content,
+                    type: statusArray[option].type,
+                },
+            ],
+
+            status: statusArray[option].status,
+        });
+
+    };
 });
 
 
