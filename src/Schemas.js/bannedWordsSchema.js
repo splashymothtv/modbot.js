7c43b90e8360ad2eb31df00f67d8b05a83fa8379
splashymothtv <75794879+splashymothtv@users.noreply.github.com>
2023-06-29 16:51:47 +0200

    Delete bannedWordsSchema.js

--- src/Schemas.js/bannedWordsSchema.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-const bannedWordsSchema = new Schema({
-  Guild: String,
-  Channel: String,
-  Msg: String,
-  Role: String,
-});
-
-module.exports = model("bannedWordsSchema", bannedWordsSchema);
