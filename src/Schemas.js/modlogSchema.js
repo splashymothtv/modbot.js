7967776f850e12802e83dc8ba788d82360fc0688
splashymothtv <75794879+splashymothtv@users.noreply.github.com>
2023-06-29 16:51:57 +0200

    Delete modlogSchema.js

--- src/Schemas.js/modlogSchema.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let modlogSchema = new Schema({
-  guildId: { type: String, required: true },
-  logChannelId: { type: String, required: true },
-});
-
-module.exports = model("modlog", modlogSchema);
