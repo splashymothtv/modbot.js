09ba416b18dc63557b036035ddb2f5559399a171
splashymothtv <75794879+splashymothtv@users.noreply.github.com>
2023-06-29 16:52:06 +0200

    Delete msglogSchema.js

--- src/Schemas.js/msglogSchema.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let msglogSchema = new Schema({
-  guildId: { type: String, required: true },
-  msgChannelId: { type: String, required: true },
-  Msg: { type: String, required: true },
-});
-
-module.exports = model("msglog", msglogSchema);
